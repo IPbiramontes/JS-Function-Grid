b5fa652e6b62ad265c9db9b8f93aba04fab58a48
Isabela Biramontes <ibiramontes.student@hsfi.us>
2017-01-17 21:43:13 +0000

    I made a smiley face and the letter s

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 //Write your function calls below this line
+/*
+Smiley face
+setColor(3, 2, "black");
+setColor(3, 6, "black");
+setColor(5, 1, "black");
+setColor(6, 2, "black");
+setColor(7, 3, "black");
+setColor(7, 4, "black");
+setColor(7, 5, "black");
+setColor(6, 6, "black");
+setColor(5, 7, "black");
+*/
+/*
+Letter S
+setColor(2, 6, "blue")
+setColor(1, 5, "blue")
+setColor(1, 4, "blue")
+setColor(2, 3, "blue")
+setColor(3, 4, "blue")
+setColor(4, 5, "blue")
+setColor(5, 6, "blue")
+setColor(2, 6, "blue")
+setColor(6, 5, "blue")
+setColor(6, 4, "blue")
+setColor(5, 3, "blue")
+*/
